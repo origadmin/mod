193393a38b8e828021f8e36118ef0cd2a2277f24
godcong <jumbycc@163.com>
2024-04-16 02:00:26 +0800

    feat(v1): add api module

---
+++ config.go
@@ -0,0 +1,4 @@
+package mod
+
+type Config struct {
+}
--- mods.go
+++ mods.go
@@ -2,6 +2,7 @@ package mod
 
 import (
 	"context"
+	"fmt"
 	"sync"
 
 	"github.com/gin-gonic/gin"
@@ -9,33 +10,47 @@ import (
 
 // Modular is an interface that defines the methods that a module must implement
 type Modular interface {
-	Name() string
 	AutoMigrate(ctx context.Context) error
-	Init(ctx context.Context) error
+	Init(ctx context.Context, config Config) error
 	Release(ctx context.Context) error
 	RegisterRouters(ctx context.Context, version string, router *gin.RouterGroup) error
 }
 
 var (
-	mods  = map[string]Modular{}
-	mutex = sync.Mutex{}
+	mods   = map[string]Modular{}
+	modsMu = sync.Mutex{}
 )
 
-func Register(modular Modular) {
-	mutex.Lock()
-	defer mutex.Unlock()
-	if _, ok := mods[modular.Name()]; ok {
-		panic("modular already registered: " + modular.Name())
+func Register(name string, modular Modular) {
+	modsMu.Lock()
+	defer modsMu.Unlock()
+	if modular == nil {
+		panic("mods: Register module is nil")
 	}
-	mods[modular.Name()] = modular
+	if _, ok := mods[name]; ok {
+		panic("mods: Register called twice for module " + name)
+	}
+	mods[name] = modular
 }
 
+// LoadModules loads all modules
 func LoadModules() []Modular {
 	var modules []Modular
-	mutex.Lock()
-	defer mutex.Unlock()
+	modsMu.Lock()
 	for _, mod := range mods {
 		modules = append(modules, mod)
 	}
+	modsMu.Unlock()
 	return modules
+}
+
+// Load loads a module by name
+func Load(name string) (Modular, error) {
+	modsMu.Lock()
+	m, ok := mods[name]
+	modsMu.Unlock()
+	if !ok {
+		return nil, fmt.Errorf("sql: unknown module %q (forgotten import?)", name)
+	}
+	return m, nil
 }
